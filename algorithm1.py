86abe7205bd61f999cdc850c90a8cf0504c01be0
oscarwohlfahrt <oscarjhw@gmail.com>
2025-12-17 17:00:50 +0100

    implement algo 1 in a general way

---
+++ algorithm1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def algorithm1(N, prior, model, M, discrep, data, eps):
+    theta = []
+    n_attempts = 0
+
+    while len(theta) < N:
+        theta_star = prior()
+        gen_data = model(theta_star, M)
+        diff = discrep(gen_data, data)
+        n_attempts += 1
+
+        if diff < eps:
+            theta.append(theta_star)
+    return theta, n_attempts
+
+
+
+
+
