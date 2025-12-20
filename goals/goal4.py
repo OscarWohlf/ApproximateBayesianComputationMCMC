06f71c1cd28379884f7310b53f94ebf9e3177554
oscarwohlfahrt <oscarjhw@gmail.com>
2025-12-20 12:40:37 +0100

    sample from prior

--- goals/goal4.py
+++ goals/goal4.py
@@ -11,6 +11,13 @@ def b_goal4(x_n, t_n):
 def sig_goal4(x_n, t_n):
     return 0.2
 
+def sample_theta_goal4():
+    log_Ke = np.random.normal(-2.7, 0.6)
+    log_Ka = np.random.normal(0.14, 0.4)
+    log_Cl = np.random.normal(-3,0.8)
+    log_sig = np.random.normal(-1.1, 0.3)
+    return np.array([np.exp(log_Ke),np.exp(log_Ka),np.exp(log_Cl), np.exp(log_sig)])
+
 def main():
     xs, ts = euler_maruyama(0.01, 12, b_goal4, sig_goal4)
     print(xs)
