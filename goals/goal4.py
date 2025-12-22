9b9cc1910350be110ceea161c2d77379c56cb7f3
oscarwohlfahrt <oscarjhw@gmail.com>
2025-12-22 14:32:41 +0100

    regression for betas and definition f summary statistic

--- goals/goal4.py
+++ goals/goal4.py
@@ -21,16 +21,43 @@ def sample_theta_goal4():
     log_sig = np.random.normal(-1.1, 0.3)
     return np.array([np.exp(log_Ke),np.exp(log_Ka),np.exp(log_Cl), np.exp(log_sig)])
 
-def simulate_dataset_em(theta):
+def simulate_dataset_em(theta, dt):
     times = np.array([0.25,0.5,1,2,3.5,5,7,9,12])
-    dt = 0.01
     xs, ts = euler_maruyama(dt, 12, b_goal4, sig_goal4, theta)
-    idx = (times / dt).astype(int)
+    idx = np.round((times / dt).astype(int))
     return xs[idx]
 
+def create_dataset(n, dt):
+    X = np.zeros((n,10))
+    Y = np.zeros((n,4))
+
+    for i in range(n):
+        theta_i = sample_theta_goal4()
+        D_i = simulate_dataset_em(theta_i, dt)
+        X[i,0] = 1.0
+        X[i, 1:] = D_i
+        Y[i,:] = theta_i
+    
+    return X, Y
+
+def get_betas(X, Y):
+    betas, *_ = np.linalg.lstsq(X,Y)
+    return betas
+
+def summary_stat(data, betas):
+    x = np.zeros(10)
+    x[0] = 1.0
+    x[1:] = data
+    return x @ betas
+    
 def main():
-    theta = sample_theta_goal4()
-    print(simulate_dataset_em(theta))
+    given_theta = np.array([0.08,1.5,0.04,0.2])
+    data_obs = simulate_dataset_em(given_theta, 0.01)
+    print(data_obs)
+    X, Y = create_dataset(1000, 0.01)
+    print(X.shape)
+    print(Y.shape)
+    print(get_betas(X,Y))
 
 if __name__ == "__main__":
     main()
