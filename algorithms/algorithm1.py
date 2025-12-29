33c16fef3d19a9908214c2e85ea017bf1c28c0b9
oscarwohlfahrt <oscarjhw@gmail.com>
2025-12-29 17:02:00 +0100

    documentation goal 1

--- algorithms/algorithm1.py
+++ algorithms/algorithm1.py
@@ -1,15 +1,25 @@
 import numpy as np
 
 def algorithm1(N, prior, model, M, discrep, data, eps):
+    """
+    Simple ABC Algorithm
+    """
     theta = []
     n_attempts = 0
 
+    # Keep proposing from the prior until we have N accepted samples
     while len(theta) < N:
+        # Propose a parameter from the prior
         theta_star = prior()
+
+        # Simulate dataset
         gen_data = model(theta_star, M)
+
+        # Compute discrepancy between simulated and observed data
         diff = discrep(gen_data, data)
         n_attempts += 1
 
+        # Accept the sample if it is within the tollerance
         if diff < eps:
             theta.append(theta_star)
     return np.asarray(theta), n_attempts
