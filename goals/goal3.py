c6dd96d4812ab17653dc47837874ed59750aa3db
PabloSarro <pablo.sarro@estudiants.urv.cat>
2026-01-11 15:37:34 +0100

    Additional documentation on goals 3 and 4.

--- goals/goal3.py
+++ goals/goal3.py
@@ -17,7 +17,7 @@ def q_proposal(theta,variance):
 
 def pi_density(theta):
     """
-    ...
+    Return the value of the N(0, 3) probability density function evaluated at theta.
     """
     return norm.pdf(theta, 0, np.sqrt(3))
 
@@ -59,38 +59,51 @@ def calculate_ESS(chain, M=100):
 
 
 def main():
-    check_ESS = 2000  # Check the ESS of the current chain every 500 iterations.
-    max_iter = 200000
+    """
+    Run Algorithm 2 for several ABC tolerance values, compare the resulting
+    approximate posterior distributions with the true posterior, and visualise
+    the effect of the proposed tolerances on the acceptance rate and posterior accuracy.
+    """
+
+    # Setting seed for reproducibility
+    np.random.seed(42)
+
+    check_ESS = 2000  # Check the ESS of the current chain every 2000 iterations.
+    max_iter = 200000 # Maximum number of iterations allowed before stopping the algorithm (in case ESS doesn't reach 500)
     M = 100
     data = [0.0]
 
-    eps = [0.75, 0.25, 0.1, 0.025]
-    var = [1, 0.3, 0.25, 0.25]
+    eps = [0.75, 0.25, 0.1, 0.025] # Tolerances considered
+    var = [1, 0.3, 0.25, 0.25] # Corresponding proposed variances
 
     grid = np.linspace(-3, 3, 1000)
     true_pdf = true_posterior_goal1(grid)
+    bin_edges = np.linspace(-2, 1, 100)
 
     fig, axes = plt.subplots(2, 2, figsize=(12, 8), sharex=True, sharey=True)
 
     for idx, e in enumerate(eps):
         print(f"Plot {idx+1}: ε={e}")
         v = var[idx]
+        # Run Algorithm 2 (ABC-based) and collect the chain and the number of accepted samples
         thetas, accepted = algorithm2(
             check_ESS, v, q_proposal, pi_density, model_goal1, M, calculate_ESS, abs_diff_discrepancy, data, e, max_iter
         )
-        acc_rate = accepted / len(thetas)
+        acc_rate = accepted / len(thetas) # acceptance rate.
 
+        # Plot of the results
         ax = axes[idx//2, idx%2]
-        ax.hist(thetas, bins=40, density=True, alpha=0.6, label="ABC accepted θ")
+        ax.hist(thetas, bins=bin_edges, density=True, alpha=0.6, label="ABC accepted θ")
         ax.plot(grid, true_pdf, label="True posterior pdf")
-        ax.set_title(rf"ε={e} | $\nu^2$={v} | acc={acc_rate:.3f}")
-        ax.set_xlabel("θ")
+        ax.set_title(rf"$\epsilon={e}$ | $\nu^2={v}$ | acc={acc_rate:.3f}")
+        ax.set_xlim([-2, 1])
+        ax.set_xlabel(r"$\theta$")
         ax.set_ylabel("density")
         ax.legend()
 
     plt.tight_layout()
-    # plt.savefig(f"ResultsAlgo2.png")
     plt.show()
+
 
 if __name__ == "__main__":
     main()
